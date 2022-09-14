b8906937511de27f014fae1ef9fe125ab2f7410d
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-09-14 13:18:48 +0800

    更新package-lock

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/');
-    cy.contains('h1', 'Welcome to Your Vue.js App');
+    cy.contains('h1', 'hello i18n !!');
   });
 });

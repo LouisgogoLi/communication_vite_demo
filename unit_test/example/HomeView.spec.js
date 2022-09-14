9d5e52531e7c54f943be4be92636c34f60ef8276
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-09-14 11:10:53 +0800

    更新unit test

--- unit_test/example/HomeView.spec.js
+++ unit_test/example/HomeView.spec.js
@@ -1,7 +1,6 @@
 import { mount } from '@vue/test-utils';
 import HomeView from '@/views/HomeView.vue';
 import { createTestingPinia } from '@pinia/testing';
-import { useCommonStore } from '@/stores/common';
 import i18n from '@/i18n.js';
 
 describe('HomeView.vue', () => {
@@ -11,7 +10,6 @@ describe('HomeView.vue', () => {
         plugins: [
           createTestingPinia({
             stubActions: false,
-            plugins: [useCommonStore],
           }),
           i18n,
         ],

36bbdeab400d1387492d2acfd32b57569b00ac20
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-09-16 05:32:52 +0000

    Deploying to gh-pages from  @ 949fc12a668363b4f5523c475322b4190db475bd 🚀

---
+++ assets/golden-goldSummaryTransDetails-legacy.e6e8725e.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.4dd09ad8.js"],(function(e,u){"use strict";var n,r;return{setters:[e=>{n=e.aM,r=e.r}],execute:function(){e("u",n("golden/goldSummaryTransDetails",(()=>{const e=r(10);return{summary:e,addSummary:function(){e.value++},minusSummary:function(){e.value--}}})))}}}));
---
+++ assets/logo-legacy.6bcb8edf.js
@@ -0,0 +1,1 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){e("_","/communication_vite_demo/assets/logo.03d6d6da.png")}}}));
